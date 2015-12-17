a5f27fe55c116327bb0640474b432daa7c2f268b
dudu.bernhard <dudu.bernhard@gmail.com>
2015-12-17 19:56:17 +0200

    aaa

--- FlatFXWebClient/JS/Functions/NotyFunctions.js
+++ FlatFXWebClient/JS/Functions/NotyFunctions.js
@@ -2,8 +2,8 @@
 //type: 'alert', 'information', 'error', 'warning', 'notification', 'success'
 
 notyWrapper = {
-    generateAlertPlusButton : function generate(container, type, text) {
-        
+    generateAlertPlusButton: function generate(container, type, text) {
+
         var n = $(container).noty({
             text: text,
             type: type,
@@ -11,6 +11,38 @@ notyWrapper = {
             layout: 'topCenter',
             theme: 'defaultTheme',
             maxVisible: 10
+
+            //layout: 'top',
+            //theme: 'defaultTheme',
+            //type: 'alert',
+            //text: '',
+            //dismissQueue: true,
+            //template: '<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>',
+            //animation: {
+            //    open: { height: 'toggle' },
+            //    close: { height: 'toggle' },
+            //    easing: 'swing',
+            //    speed: 500
+            //},
+            //timeout: false,
+            //force: false,
+            //modal: false,
+            //maxVisible: 5,
+            //killer: false,
+            //closeWith: ['click'],
+            //callback: {
+            //    onShow: function () {
+            //    },
+            //    afterShow: function () {
+            //    },
+            //    onClose: function () {
+            //    },
+            //    afterClose: function () {
+            //    },
+            //    onCloseClick: function () {
+            //    }
+            //},
+            //buttons: false
         });
     }
 }
