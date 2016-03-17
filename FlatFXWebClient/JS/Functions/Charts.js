269e31e67c7fc124121c529517391d1c38d38df9
dudu1234 <dudu.bernhard@g,ail.com>
2016-03-17 10:43:17 +0200

    mobile responsive phase 2

--- FlatFXWebClient/JS/Functions/Charts.js
+++ FlatFXWebClient/JS/Functions/Charts.js
@@ -63,10 +63,10 @@ Chart.defaults.global = {
     scaleFontColor: "#666",
 
     // Boolean - whether or not the chart should be responsive and resize when the browser does.
-    responsive: false,
+    responsive: true,
 
     // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-    maintainAspectRatio: true,
+    maintainAspectRatio: false,
 
     // Boolean - Determines whether to draw tooltips on the canvas or not
     showTooltips: true,
